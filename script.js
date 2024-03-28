53ce58d433ba8ab8bb0c71be2be4cf52f214cf9a
myhrindra194 <myhrindra194@gmail.com>
2024-03-28 19:15:14 +0300

    first version of bp

---
+++ script.js
@@ -0,0 +1,67 @@
+const buttonBudget = document.querySelector(".setBudget");
+const inputBudget = document.querySelector(".inputBudget");
+
+const inputLabel = document.querySelector(".inputLabel");
+const inputCost = document.querySelector(".inputCost");
+const buttonAddList = document.querySelector(".addList");
+
+const expenseValue = document.querySelector(".expenseValue");
+const balanceValue = document.querySelector(".balanceValue");
+
+balanceValue.textContent = parseFloat("0");
+expenseValue.textContent = parseFloat("0");
+document.querySelector(".totalValue").textContent = parseFloat("0");
+
+function getDate(){
+    let date = new Date().toString().split(" ");
+    return date[0] +" "+ date[1] + " "+ date[2];
+}
+
+
+const addBudget = () => {
+    if(inputBudget.value != ""){
+        balanceValue.textContent = parseFloat(inputBudget.value);
+        document.querySelector(".totalValue").textContent = inputBudget.value;
+        inputBudget.value = "";
+    }
+}
+const addList = () => {
+    if(inputLabel.value.trim() != "" && inputCost.value != "" && inputCost.value < parseInt(balanceValue.textContent)){
+        expenseValue.textContent = parseInt(expenseValue.textContent) + parseInt(inputCost.value); 
+        balanceValue.textContent = parseInt(balanceValue.textContent) - parseInt(inputCost.value);
+        
+        let item = `<li class="list-group-item ">
+        <div class="">
+        <span class="text-muted">${getDate()}</span>
+        <div class="row d-flex justify-content-between align-items-center">
+            <div class="fw-bold col-10 d-flex">
+                <span class="border-start border-primary border-4"></span>
+                <h5 class= "mx-3">${inputLabel.value.trim()}</h5>
+            </div>
+            <div class="col-2">
+                <h5 class="text-muted">${inputCost.value}</h5>
+            </div>
+        </div>
+
+        </div>
+      </li>`;
+
+        document.querySelector(".list-group").innerHTML += item;
+        
+
+        inputLabel.value = "";
+        inputCost.value = "";
+    }
+}
+
+buttonBudget.addEventListener("click",() => {
+    addBudget();
+});
+
+
+buttonAddList.addEventListener("click", () => {
+    addList();
+});
+
+
+
